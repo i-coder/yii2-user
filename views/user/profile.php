659483e4808046634154f497f2ef9c4312f0795a
Роман <coder.ept@gmail.com>
2016-10-31 17:43:05 +0500

    user

--- views/user/profile.php
+++ views/user/profile.php
@@ -53,10 +53,26 @@ $assets = UserAsset::register($this);
 
     <div class="col-lg-6">
 
-        <?= $form->field($model, 'first_name')->textInput([
-            'maxlength' => true,
-            'placeholder' => $model->getAttributeLabel('first_name')
-        ]) ?>
+        <div class="row">
+            <div class="col-md-4">
+                <?= $form->field($model, 'first_name')->textInput([
+                    'maxlength' => true,
+                    'placeholder' => $model->getAttributeLabel('first_name')
+                ]);?>
+            </div>
+            <div class="col-md-4">
+                <?= $form->field($model, 'last_name')->textInput([
+                    'maxlength' => true,
+                    'placeholder' => $model->getAttributeLabel('last_name')
+                ]);?>
+            </div>
+            <div class="col-md-4">
+                <?= $form->field($model, 'patronymic')->textInput([
+                    'maxlength' => true,
+                    'placeholder' => $model->getAttributeLabel('patronymic')
+                ]);?>
+            </div>
+        </div>
 
 <!--        --><?//= $form->field($model, 'last_name')->textInput([
 //            'maxlength' => true,
